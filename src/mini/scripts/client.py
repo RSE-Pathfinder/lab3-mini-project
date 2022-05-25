07b462579ded1dc0d1b4d6dde6821e99c85ec9a0
Muhammad_Syamim <Syazam33@gmail.com>
2022-05-25 15:56:33 +0700

    Updated client.py

--- src/mini/scripts/client.py
+++ src/mini/scripts/client.py
@@ -6,50 +6,32 @@
 # uint8 GET_STATUS_ERROR_CODE = 3
 # uint8 GET_STATUS_MOTION_MODE = 4
 
+# IFDEBUG
+from __future__ import print_function
 import rospy
 from std_msgs.msg import String
 from mini.srv import *
 
-size = 30
-req_val = 0
-
 def req():
   #Initialise Node
   rospy.init_node('client_node', anonymous=True)
+  size = 30
+  req_val = 0
 
   #Waits for active Status service
+  print("Waiting for service")
   rospy.wait_for_service('Status')
   rospy.Rate(1)
 
-  try:
-      #Cycle commands
-      req_val+=1
-      if req_val > 4:
-        req_val = 0
+  #Cycle commands
+  req_val+=1
+  if req_val > 4:
+    req_val = 0
 
-      #Send service request
-      get = rospy.ServiceProxy('Status', Status)
-      limo_status = get(req_val)
-
-      #Publish result
-      if req_val == 0:
-        pub0 = rospy.Publisher('limo_status/vehicle_state', String, queue_size = size)
-        pub0.publish(limo_status)
-      elif req_val == 1:
-        pub1 = rospy.Publisher('limo_status/control_mode', String, queue_size = size)
-        pub1.publish(limo_status)
-      elif req_val == 2:
-        pub2 = rospy.Publisher('limo_status/vehicle_state', String, queue_size = size)
-        pub2.publish(limo_status)
-      elif req_val == 3:
-        pub3 = rospy.Publisher('/limo_status/error_code', String, queue_size = size)
-        pub3.publish(limo_status)
-      elif req_val == 4:
-        pub4 = rospy.Publisher('/limo_status/motion_mode', String, queue_size = size)
-        pub4.publish(limo_status)
-
-  except:
-      return
+  #Send service request
+  print("Requesting service")
+  get = rospy.ServiceProxy('Status', Status)
+  limo_status = get(req_val)
 
   rospy.spin()
 
