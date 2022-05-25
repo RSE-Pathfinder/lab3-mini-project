4602d330103ec3e2c5dce5afd0690a2e299ff640
yongpaulsit <2102088@sit.singaporetech.edu.sg>
2022-05-25 17:31:36 +0800

    Modified script/server.py

--- src/mini/scripts/server.py
+++ src/mini/scripts/server.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python
 
 #IFDEBUG
-from __future__ import print_function
-from mini.srv import *
+from __future__ import print_function 
+#ENDIF
+
 import rospy
 from std_msgs.msg import String
 from mini.srv import Status
 
 def handle_status(req):
+    if req.get_status == 0:
+        myStatus = "GET_STATUS_VEHICLE_STATE"
+    elif req.get_status == 1:
+        myStatus = "GET_STATUS_CONTROL_MODE"
+    elif req.get_status == 2:
+        myStatus = "GET_STATUS_BATTERY_VOLTAGE"
+    elif req.get_status == 3:
+        myStatus = "GET_STATUS_ERROR_CODE"
+    elif req.get_status == 4:
+        myStatus = "GET_STATUS_MOTION_MODE"
+
     #IFDEBUG
-    print("Returning.")
-    return {
-        '0': Status("GET_STATUS_VEHICLE_STATE"),
-        '1': Status("GET_STATUS_CONTROL_MODE"),
-        '2': Status("GET_STATUS_BATTERY_VOLTAGE"),
-        '3': Status("GET_STATUS_ERROR_CODE"),
-        '4': Status("GET_STATUS_MOTION_MODE"),
-    } [Status]
+    print("Returning Status.")
+    #ENDIF
+
+    return myStatus
 
 def status_server():
     rospy.init_node('status_server')
     s = rospy.Service('Status', Status, handle_status)
+
     #IFDEBUG
-    print("This is server.")
+    print("This is server.") 
+    #ENDIF
+
     rospy.spin()
     
 if __name__ == "__main__":
