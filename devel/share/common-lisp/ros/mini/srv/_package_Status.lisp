46ba78c012778bccdfca84e7690dd5a3ddc1e605
Muhd Syamim <Syazam33@gmail.com>
2022-05-25 11:44:40 +0700

    Delete devel directory

--- devel/share/common-lisp/ros/mini/srv/_package_Status.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package mini-srv)
-(cl:export '(GET_STATUS-VAL
-          GET_STATUS
-          STATUS_STRING-VAL
-          STATUS_STRING
-))
--- devel/share/mini/cmake/mini-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(mini_MSG_INCLUDE_DIRS "")
-set(mini_MSG_DEPENDENCIES std_msgs)
