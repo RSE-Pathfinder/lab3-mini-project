0dad7e451cd09ee82e5ca6bb713604ab99b2a45a
yongpaulsit <2102088@sit.singaporetech.edu.sg>
2022-05-25 11:25:56 +0800

    Initial commit by Paul

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/paulyse/lab3-mini-project/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/melodic/bin:/home/paulyse/gems/bin:/home/paulyse/.local/bin:/home/paulyse/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/paulyse/lab3-mini-project/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/paulyse/lab3-mini-project/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/paulyse/lab3-mini-project/src:$ROS_PACKAGE_PATH"
