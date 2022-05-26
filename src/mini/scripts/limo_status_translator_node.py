57df7338c62554e699e340a88aed2566bf7be0f2
yongpaulsit <2102088@sit.singaporetech.edu.sg>
2022-05-26 14:09:47 +0800

    Modified src/mini/scripts/limo_status_translator_node.py

--- src/mini/scripts/limo_status_translator_node.py
+++ src/mini/scripts/limo_status_translator_node.py
@@ -6,35 +6,47 @@ from __future__ import print_function
 
 import rospy
 from std_msgs.msg import String
-from limo_base import LimoStatus
-from mini.srv import Status
-import numpy as np
+##  String data type
 
-vehicle_state = 0
-control_mode = 0
-battery = 0.0
-#error_code is an array
-error_code = 0
-motion_mode = 0
+from limo_base import LimoStatus
+##  LimoStatus.msg
+#   Header header
+#
+#   uint8 vehicle_state
+#   uint8 control_mode
+#   float64 battery_voltage
+#   uint16 error_code
+#   uint8 motion_mode
+##  END
+
+from mini.srv import Status
+##  Status.srv
+#   #request arguement
+#   uint8 get_status
+#   ---
+#   #response
+#   string status_string
+##  END
 
 def callback(data):
-    global vehicle_state
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %d', data.vs)
-    vehicle_state = np.uint8(data.vs)
-    LimoStatus.vehicle_state
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %d', data)
+
+    #IFDEBUG
+    print("Translating Status.")
+    #ENDIF
 
 def handle_status(req):
     myStatus = ""
     if req.get_status == 0:
-        myStatus = "GET_STATUS_VEHICLE_STATE"
+        myStatus = "GET_STATUS_VEHICLE_STATE %s" % LimoStatus.vehicle_state
     elif req.get_status == 1:
-        myStatus = "GET_STATUS_CONTROL_MODE"
+        myStatus = "GET_STATUS_CONTROL_MODE %s" % LimoStatus.control_mode
     elif req.get_status == 2:
-        myStatus = "GET_STATUS_BATTERY_VOLTAGE"
+        myStatus = "GET_STATUS_BATTERY_VOLTAGE %s" % LimoStatus.battery_voltage
     elif req.get_status == 3:
-        myStatus = "GET_STATUS_ERROR_CODE"
+        myStatus = "GET_STATUS_ERROR_CODE %s" % LimoStatus.error_code
     elif req.get_status == 4:
-        myStatus = "GET_STATUS_MOTION_MODE"
+        myStatus = "GET_STATUS_MOTION_MODE %s" % LimoStatus.motion_mode
 
     #IFDEBUG
     print("Returning Status.")
@@ -43,8 +55,8 @@ def handle_status(req):
     return myStatus
 
 def status_server():
-    rospy.init_node('status_server', anonymous = True)
-    rospy.Subscriber('/limo_status', uint8, callback)
+    rospy.init_node('limo_status_translator_node', anonymous = True)
+    rospy.Subscriber('/limo_status', LimoStatus, callback)
     s = rospy.Service('Status', Status, handle_status)
 
     #IFDEBUG
@@ -54,5 +66,7 @@ def status_server():
     rospy.spin()
 
 if __name__ == '__main__':
-    limo_translator_node()
-    status_server()
+    try:
+        status_server()
+    except rospy.ROSInterruptException:
+        pass
